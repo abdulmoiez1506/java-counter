67da06e24cf0ed060e3a09bdad04ac7d187e22a4
abdul moiez <155585384+abdulmoiez1506@users.noreply.github.com>
2024-02-07 17:57:38 +0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,30 @@
+var greetings = alert("WELLCOME TO OUR WEB-PAGE")
+
+
+const mainTitle = document.querySelector('#title');
+let curValue = 0;
+
+const btnDecrement = document.querySelector('#decrement');
+const btnReset = document.querySelector('#reset');
+const btnIncrement = document.querySelector('#increment');
+
+
+btnIncrement.addEventListener('click', () => {
+  curValue++;
+  mainTitle.textContent = curValue;
+});
+
+btnDecrement.addEventListener('click', () => {
+  curValue--;
+  mainTitle.textContent = curValue;
+});
+
+btnReset.addEventListener('click', () => {
+  curValue = 0;
+  mainTitle.textContent = curValue;
+});
+
+
+
+
+
